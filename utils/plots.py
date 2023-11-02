8aec12984e4d045817d3ea0840a8287bd0cdc98e
desafin <131871057+desafin@users.noreply.github.com>
2023-11-02 12:36:09 +0900

    final

--- utils/plots.py
+++ utils/plots.py
@@ -60,12 +60,22 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=3):
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+
+    # Calculate the center of the bounding box
+    center = ((c1[0] + c2[0]) // 2, (c1[1] + c2[1]) // 2)
+
+    # Draw a red point (circle) at the center of the bounding box
+    cv2.circle(img, center, 3, (0, 0, 255), -1)  # Red color
+
+    print(center)
     if label:
         tf = max(tl - 1, 1)  # font thickness
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
         cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
         cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+
 
 
 def plot_one_box_PIL(box, img, color=None, label=None, line_thickness=None):
